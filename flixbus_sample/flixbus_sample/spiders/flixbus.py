3a40817e6e25f922e2ce0f800c2731880343241a
Mohammad Bana <mohamad.bana.1380@gmail.com>
2023-06-05 20:00:36 +0330

    get correct data from flixbus

--- flixbus_sample/flixbus_sample/spiders/flixbus.py
+++ flixbus_sample/flixbus_sample/spiders/flixbus.py
@@ -1,17 +1,37 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+import json
 
 
 class FlixbusSpider(scrapy.Spider):
     name = "flixbus"
     allowed_domains = ["shop.global.flixbus.com"]
     # start_urls = ["https://global.flixbus.com"]
+    required_parameters = [
+        'from_city_id',
+        'to_city_id',
+        'date',
+    ]
     
     def __init__(self, from_city_id=None, to_city_id=None, date=None, *args, **kwargs):
         super(FlixbusSpider, self).__init__(*args, **kwargs)
-        self.start_urls = [f"https://shop.global.flixbus.com/search?departureCity={from_city_id}&arrivalCity={to_city_id}&rideDate={date}&adult=1"]
+        self.start_urls = [f"https://global.api.flixbus.com/search/service/v4/search?from_city_id={from_city_id}&to_city_id={to_city_id}&departure_date={date}&products=%7B%22adult%22%3A1%7D&currency=EUR&locale=en&search_by=cities&include_after_midnight_rides=1"]
 
     def parse(self, response):
         print("--------------------------------$$$##########$$$$$$$$$$$$$######")
-        print(response)
+        try:
+            data = json.loads(response.text)
+            cities = data["cities"]
+            brands = data["brands"]
+            operators = data["operators"]
+            stations = data["stations"]
+            trips = data["trips"]
+            yield {
+                "cities": cities,
+                "brands": brands,
+                "operators": operators,
+                "stations" : stations,
+                "trips": trips,
+            }
+        except Exception as e:
+            print(f"Error: {e}")
         print("--------------------------------$$$##########$$$$$$$$$$$$$######")
